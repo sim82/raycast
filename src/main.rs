e2267cc64ef35b17e6b6f7d4a7fc083e2b313042
Simon A. Berger <simberger@gmail.com>
2023-03-10 19:48:12 +0100

    load things, derive sprites

--- src/main.rs
+++ src/main.rs
@@ -731,11 +731,17 @@ fn draw_sprite(
     }
 }
 
+#[derive(Clone, Copy, Debug)]
+enum Directionality {
+    Direction(Direction),
+    Undirectional,
+}
+
 struct Sprite {
     x: Fp16,
     y: Fp16,
     id: i32,
-    directional: bool,
+    directionality: Directionality,
 }
 
 struct Sprites {
@@ -755,7 +761,7 @@ impl Default for Sprites {
                             x: FP16_HALF + (x as i32).into(),
                             y: FP16_HALF + (y as i32).into(),
                             id: 51, //(*c - b'0') as i32,
-                            directional: true,
+                            directionality: Directionality::Direction(Direction::N),
                         })
                     } else {
                         None
@@ -787,7 +793,7 @@ impl Sprites {
                         x: FP16_HALF + x.into(),
                         y: FP16_HALF + y.into(),
                         id: (c - props_range.start() + props_sprite_offset) as i32,
-                        directional: false,
+                        directionality: Directionality::Undirectional,
                     })
                 }
             }
@@ -821,7 +827,7 @@ impl Sprites {
             // let screen_x = (WIDTH as i32 / 2) + (ty * (WIDTH as i32 / 2)).get_int();
             let screen_x = ((FP16_ONE + (ty / z)) * (WIDTH as i32 / 2)).get_int();
 
-            let id = if sprite.directional {
+            let id = if let Directionality::Direction(direction) = sprite.directionality {
                 // calculate sprite orientation from player view angle
                 // 1. rotate sprite by inverse of camera rotation
                 // 2. turn by 'half an octant' to align steps to expected position
@@ -838,7 +844,8 @@ impl Sprites {
                     viewangle -= FA_STEPS as i32;
                 }
                 viewangle /= FA_STEPS as i32 / 8;
-                sprite.id + viewangle
+                viewangle += direction.sprite_offset();
+                sprite.id + viewangle % 8
             } else {
                 sprite.id
             };
@@ -857,8 +864,92 @@ impl Sprites {
     }
 }
 
+#[derive(Clone, Copy, Debug)]
+enum Difficulty {
+    Easy,
+    Medium,
+    Hard,
+}
+
+#[derive(Clone, Copy, Debug)]
+enum EnemyType {
+    Brown,
+    White,
+    Blue,
+    Woof,
+    Rotten,
+}
+
+impl EnemyType {
+    pub fn sprite_offset(&self) -> i32 {
+        match self {
+            EnemyType::Brown => 51,
+            EnemyType::White => 51 + 49 + 49 + 49 + 49,
+            EnemyType::Blue => 51 + 49 + 49,
+            EnemyType::Woof => 51 + 49,
+            EnemyType::Rotten => 51 + 49 + 49 + 49,
+        }
+    }
+}
+
+enum AnimationState {
+    Stand,
+    Walk1,
+    Walk2,
+    Walk3,
+    Walk4,
+}
+
+impl AnimationState {
+    pub fn sprite_offset(&self) -> i32 {
+        match self {
+            AnimationState::Stand => 0,
+            AnimationState::Walk1 => 8,
+            AnimationState::Walk2 => 16,
+            AnimationState::Walk3 => 24,
+            AnimationState::Walk4 => 32,
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+enum EnemyState {
+    Standing,
+    Patrolling,
+}
+
+#[derive(Clone, Copy, Debug)]
 enum ThingType {
     PlayerStart(i32),
+    Enemy(Direction, Difficulty, EnemyType, EnemyState),
+}
+
+#[derive(Clone, Copy, Debug)]
+enum Direction {
+    N,
+    E,
+    S,
+    W,
+}
+
+impl Direction {
+    pub fn angle(&self) -> i32 {
+        match &self {
+            Direction::N => FA_PI_FRAC_PI_2,
+            Direction::E => 0,
+            Direction::S => FA_FRAC_PI_2,
+            Direction::W => FA_PI,
+            _ => panic!(),
+        }
+    }
+    pub fn sprite_offset(&self) -> i32 {
+        match &self {
+            Direction::N => 0,
+            Direction::E => 2,
+            Direction::S => 4,
+            Direction::W => 6,
+        }
+    }
 }
 
 struct Thing {
@@ -879,12 +970,17 @@ impl Things {
         for y in 0..64 {
             for x in 0..64 {
                 let c = plane_iter.next().unwrap();
-                let thing_type = match c {
-                    19 => ThingType::PlayerStart(FA_PI_FRAC_PI_2), // NORTH means facing -y
-                    20 => ThingType::PlayerStart(0),
-                    21 => ThingType::PlayerStart(FA_FRAC_PI_2),
-                    22 => ThingType::PlayerStart(FA_PI),
-                    _ => continue,
+
+                let thing_type = if let Some(enemy) = Things::map_enemy(*c) {
+                    enemy
+                } else {
+                    match *c {
+                        19 => ThingType::PlayerStart(FA_PI_FRAC_PI_2), // NORTH means facing -y
+                        20 => ThingType::PlayerStart(0),
+                        21 => ThingType::PlayerStart(FA_FRAC_PI_2),
+                        22 => ThingType::PlayerStart(FA_PI),
+                        _ => continue,
+                    }
                 };
 
                 things.push(Thing {
@@ -897,6 +993,48 @@ impl Things {
         Things { things }
     }
 
+    fn oa(o: u16) -> Direction {
+        match o % 4 {
+            0 => Direction::N,
+            1 => Direction::E,
+            2 => Direction::S,
+            3 => Direction::W,
+            _ => panic!(),
+        }
+    }
+    fn os(o: u16) -> EnemyState {
+        if o <= 3 {
+            EnemyState::Standing
+        } else {
+            EnemyState::Patrolling
+        }
+    }
+    
+    #[rustfmt::skip]
+    fn map_enemy(t: u16) -> Option<ThingType> {
+        Some(match t {
+            // easy
+            108..=115 => ThingType::Enemy(Things::oa(t - 108), Difficulty::Easy, EnemyType::Brown, Things::os(t - 108)),
+            116..=123 => ThingType::Enemy(Things::oa(t - 116), Difficulty::Easy, EnemyType::White, Things::os(t - 116)),
+            126..=133 => ThingType::Enemy(Things::oa(t - 126), Difficulty::Easy, EnemyType::Blue, Things::os(t - 126)),
+            134..=141 => ThingType::Enemy(Things::oa(t - 134), Difficulty::Easy, EnemyType::Woof, Things::os(t - 134)),
+            216..=223 => ThingType::Enemy(Things::oa(t - 134), Difficulty::Easy, EnemyType::Rotten, Things::os(t - 216)),
+            // medium
+            144..=151 => ThingType::Enemy(Things::oa(t - 144), Difficulty::Medium, EnemyType::Brown, Things::os(t - 144)),
+            152..=159 => ThingType::Enemy(Things::oa(t - 152), Difficulty::Medium, EnemyType::White, Things::os(t - 152)),
+            162..=169 => ThingType::Enemy(Things::oa(t - 162), Difficulty::Medium, EnemyType::Blue, Things::os(t - 162)),
+            170..=177 => ThingType::Enemy(Things::oa(t - 170), Difficulty::Medium, EnemyType::Woof, Things::os(t - 170)),
+            234..=241 => ThingType::Enemy(Things::oa(t - 234), Difficulty::Medium, EnemyType::Rotten, Things::os(t - 234)),
+            // hard
+            180..=187 => ThingType::Enemy(Things::oa(t - 180), Difficulty::Hard, EnemyType::Brown, Things::os(t - 180)),
+            188..=195 => ThingType::Enemy(Things::oa(t - 188), Difficulty::Hard, EnemyType::White, Things::os(t - 188)),
+            198..=205 => ThingType::Enemy(Things::oa(t - 198), Difficulty::Hard, EnemyType::Blue, Things::os(t - 198)),
+            206..=213 => ThingType::Enemy(Things::oa(t - 206), Difficulty::Hard, EnemyType::Woof, Things::os(t - 206)),
+            252..=259 => ThingType::Enemy(Things::oa(t - 252), Difficulty::Hard, EnemyType::Rotten, Things::os(t - 252)),
+            _ => return None,
+        })
+    }
+
     pub fn get_player_start(&self) -> Option<(Fp16, Fp16, i32)> {
         for thing in &self.things {
             match thing.thing_type {
@@ -905,6 +1043,26 @@ impl Things {
             }
         }
         None
+    }
+
+    pub fn get_sprites(&self) -> Vec<Sprite> {
+        self.things
+            .iter()
+            .filter_map(|thing| match &thing.thing_type {
+                ThingType::Enemy(direction, difficulty, enemy_type, state) => {
+                    let id = enemy_type.sprite_offset()
+                        + AnimationState::Stand.sprite_offset()
+                        /*+ direction.sprite_offset()*/;
+                    Some(Sprite {
+                        id,
+                        x: thing.x,
+                        y: thing.y,
+                        directionality: Directionality::Direction(*direction),
+                    })
+                }
+                _ => None,
+            })
+            .collect()
     }
 }
 
@@ -949,10 +1107,11 @@ fn main() {
         //     rot: 3476,
         // };
 
-        let mut sprites = Sprites::from_map_plane(&plane1);
-        // let mut sprites = Sprites::default();
-
         let things = Things::from_map_plane(&plane1);
+
+        let mut sprites = Sprites::from_map_plane(&plane1);
+        sprites.sprites.append(&mut things.get_sprites());
+        // let mut sprites = Sprites::default();
 
         let mut player = things
             .get_player_start()
