973faee8d639dcad28203e93a4d215deecb0707c
Simon A. Berger <simberger@gmail.com>
2023-04-11 19:18:53 +0200

    better error handling in states_compiler

--- examples/state_compiler.rs
+++ examples/state_compiler.rs
@@ -1,24 +1,25 @@
-use std::{
-    collections::{BTreeMap, HashMap},
-    io::Write,
-};
+use std::{collections::BTreeMap, io::Write};
 
 use byteorder::{LittleEndian, WriteBytesExt};
 use nom::{
     branch::alt,
-    bytes::complete::{tag, take_while},
+    bytes::complete::{tag, take_till1, take_while},
     character::complete::{alpha1, alphanumeric1, char, multispace0, one_of},
     combinator::recognize,
-    error::ParseError,
+    error::{ErrorKind, ParseError},
     multi::{many0, many0_count, many1, separated_list0},
     sequence::{delimited, pair, terminated},
     IResult,
 };
+use nom_locate::LocatedSpan;
 use raycast::{
     ms::Writable,
     prelude::*,
     state_bc::{self, SpawnInfos},
 };
+
+type Span<'a> = LocatedSpan<&'a str>;
+type Res<'a, Output> = IResult<Span<'a>, Output, MyError<Span<'a>>>;
 
 #[derive(Debug)]
 enum ToplevelElement {
@@ -139,9 +140,43 @@ fn codegen(outname: &str, state_blocks: &[StatesBlock], enums: &BTreeMap<String,
     }
 }
 
-fn ws<'a, F, O, E: ParseError<&'a str>>(inner: F) -> impl FnMut(&'a str) -> IResult<&'a str, O, E>
+// WIP: error handling inspired by https://www.reddit.com/r/rust/comments/mtmufz/comment/gv18j0a/?utm_source=share&utm_medium=web2x&context=3
+// but much more primitive and simplified by using nom_locate.
+
+#[derive(Debug)]
+pub enum MyError<I> {
+    Nom(nom::error::Error<I>),
+    Custom(String),
+}
+
+impl<I> ParseError<I> for MyError<I> {
+    fn from_error_kind(input: I, kind: ErrorKind) -> Self {
+        // Error { input, code: kind }
+        MyError::Nom(nom::error::Error::from_error_kind(input, kind))
+    }
+
+    fn append(_: I, _: ErrorKind, other: Self) -> Self {
+        other
+    }
+}
+
+fn handle_unexpected<'a, Output, P, F>(parser: P, error_f: F) -> impl Fn(Span<'a>) -> Res<'a, Output>
 where
-    F: FnMut(&'a str) -> IResult<&'a str, O, E>,
+    P: Fn(Span<'a>) -> Res<'a, Span<'a>>,
+    F: Fn(Span<'a>) -> MyError<Span<'a>>,
+{
+    use nom::Err::{Error, Failure, Incomplete};
+    move |i: Span<'a>| match parser(i) {
+        Ok((_i_after_parse, parsed)) => Err(Failure(error_f(parsed))),
+        Err(Failure(e)) => Err(Failure(e)),
+        Err(Error(e)) => Err(Error(e)),
+        Err(Incomplete(need)) => Err(Incomplete(need)),
+    }
+}
+
+fn ws<'a, F, O>(inner: F) -> impl FnMut(Span<'a>) -> Res<O>
+where
+    F: FnMut(Span<'a>) -> Res<'a, O>,
 {
     delimited(multispace0, inner, multispace0)
 }
@@ -150,15 +185,15 @@ fn is_identifier(c: char) -> bool {
     c.is_alphanumeric() || c == '_'
 }
 
-fn parse_enum_name(input: &str) -> IResult<&str, &str> {
+fn parse_enum_name(input: Span) -> Res<'_, Span> {
     ws(take_while(is_identifier))(input)
 }
 
-fn parse_enum_body(input: &str) -> IResult<&str, Vec<&str>> {
+fn parse_enum_body(input: Span) -> Res<'_, Vec<Span>> {
     separated_list0(tag(","), parse_enum_name)(input)
 }
 
-fn parse_enum_decl(input: &str) -> IResult<&str, ToplevelElement> {
+fn parse_enum_decl(input: Span) -> Res<'_, ToplevelElement> {
     let (input, _) = ws(tag("enum"))(input)?;
     let (input, enum_names) = delimited(char('{'), parse_enum_body, char('}'))(input)?;
     Ok((
@@ -169,12 +204,12 @@ fn parse_enum_decl(input: &str) -> IResult<&str, ToplevelElement> {
     ))
 }
 
-fn parse_label(input: &str) -> IResult<&str, StatesBlockElement> {
+fn parse_label(input: Span) -> Res<'_, StatesBlockElement> {
     let (input, label_name) = ws(terminated(take_while(is_identifier), char(':')))(input)?;
     Ok((input, StatesBlockElement::Label(label_name.to_string())))
 }
 
-pub fn identifier(input: &str) -> IResult<&str, String> {
+pub fn identifier(input: Span) -> Res<'_, String> {
     recognize(pair(
         alt((alpha1, tag("_"))),
         many0_count(alt((alphanumeric1, tag("_")))),
@@ -182,16 +217,16 @@ pub fn identifier(input: &str) -> IResult<&str, String> {
     .map(|(i, s)| (i, s.to_string()))
 }
 
-fn decimal(input: &str) -> IResult<&str, i32> {
+fn decimal(input: Span) -> Res<'_, i32> {
     recognize(many1(terminated(one_of("0123456789"), many0(char('_')))))(input)
         .map(|(i, s)| (i, s.parse::<i32>().expect("failed to parse integer {s}")))
 }
 
-fn boolean(input: &str) -> IResult<&str, bool> {
-    alt((tag("true"), tag("false")))(input).map(|(i, s)| (i, s == "true"))
+fn boolean(input: Span) -> Res<'_, bool> {
+    alt((tag("true"), tag("false")))(input).map(|(i, s)| (i, *s.fragment() == "true"))
 }
 
-fn parse_state(input: &str) -> IResult<&str, StatesBlockElement> {
+fn parse_state(input: Span) -> Res<'_, StatesBlockElement> {
     let (input, _) = ws(tag("state"))(input)?;
     let (input, (id, directional, ticks, think, action, next)) = parse_state_body(input)?;
 
@@ -208,7 +243,7 @@ fn parse_state(input: &str) -> IResult<&str, StatesBlockElement> {
     ))
 }
 
-fn parse_state_body(input: &str) -> IResult<&str, (String, bool, i32, String, String, String)> {
+fn parse_state_body(input: Span) -> Res<'_, (String, bool, i32, String, String, String)> {
     let (input, id) = ws(identifier)(input)?;
     let (input, _) = char(',')(input)?;
     let (input, directional) = ws(boolean)(input)?;
@@ -224,11 +259,11 @@ fn parse_state_body(input: &str) -> IResult<&str, (String, bool, i32, String, St
     Ok((input, (id, directional, ticks, think, action, next)))
 }
 
-fn states_block_element(input: &str) -> IResult<&str, StatesBlockElement> {
+fn states_block_element(input: Span) -> Res<'_, StatesBlockElement> {
     alt((parse_label, parse_state))(input)
 }
 
-fn parse_states_block(input: &str) -> IResult<&str, ToplevelElement> {
+fn parse_states_block(input: Span) -> Res<'_, ToplevelElement> {
     let (input, _) = ws(tag("states"))(input)?;
     let (input, name) = ws(take_while(is_identifier))(input)?;
     let (input, elements) = delimited(ws(char('{')), many0(states_block_element), ws(char('}')))(input)?;
@@ -251,7 +286,7 @@ fn spawn_on_death(name: &str) -> Option<i32> {
     }
 }
 
-fn spawn_block_directional_element(input: &str) -> IResult<&str, Vec<EnemySpawnInfo>> {
+fn spawn_block_directional_element(input: Span) -> Res<'_, Vec<EnemySpawnInfo>> {
     let (input, _) = ws(tag("directional"))(input)?;
     let (input, start_id) = ws(decimal)(input)?;
     let (input, _) = char(',')(input)?;
@@ -275,7 +310,7 @@ fn spawn_block_directional_element(input: &str) -> IResult<&str, Vec<EnemySpawnI
     }
     Ok((input, infos))
 }
-fn spawn_block_undirectional_element(input: &str) -> IResult<&str, Vec<EnemySpawnInfo>> {
+fn spawn_block_undirectional_element(input: Span) -> Res<'_, Vec<EnemySpawnInfo>> {
     let (input, _) = ws(tag("undirectional"))(input)?;
     let (input, id) = ws(decimal)(input)?;
     let (input, _) = char(',')(input)?;
@@ -293,7 +328,7 @@ fn spawn_block_undirectional_element(input: &str) -> IResult<&str, Vec<EnemySpaw
     Ok((input, infos))
 }
 
-fn parse_spawn_block(input: &str) -> IResult<&str, ToplevelElement> {
+fn parse_spawn_block(input: Span) -> Res<'_, ToplevelElement> {
     let (input, _) = ws(tag("spawn"))(input)?;
     let (input, name) = ws(take_while(is_identifier))(input)?;
     let (input, mut elements) = delimited(
@@ -316,8 +351,15 @@ fn parse_spawn_block(input: &str) -> IResult<&str, ToplevelElement> {
     ))
 }
 
-fn parse_toplevel(input: &str) -> IResult<&str, ToplevelElement> {
-    alt((parse_enum_decl, parse_states_block, parse_spawn_block))(input)
+fn parse_toplevel(input: Span) -> Res<'_, ToplevelElement> {
+    alt((
+        parse_enum_decl,
+        parse_states_block,
+        parse_spawn_block,
+        handle_unexpected(take_till1(|c: char| c.is_whitespace()), |txt| {
+            MyError::Custom(format!("unexpected at toplevel: {txt:?}"))
+        }),
+    ))(input)
 }
 
 fn main() {
@@ -325,8 +367,9 @@ fn main() {
     let outname = std::env::args().nth(2).expect("missing output file");
 
     let input = std::fs::read_to_string(filename).expect("failed to read input file");
+    let input = Span::new(&input);
 
-    let (_input, toplevel_elements) = many1(parse_toplevel)(&input).expect("failed to parse toplevel elements");
+    let (_input, toplevel_elements) = many1(parse_toplevel)(input).expect("failed to parse toplevel elements");
     // println!("elements: {toplevel_elements:?}");
 
     let mut enums = BTreeMap::new();
