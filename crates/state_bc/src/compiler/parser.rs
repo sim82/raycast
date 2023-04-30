967cb382386c0c2f15e3b324b693d3398d1a8836
Simon A. Berger <simberger@gmail.com>
2023-04-30 14:23:36 +0200

    factor out parser/util

--- crates/state_bc/src/compiler/parser.rs
+++ crates/state_bc/src/compiler/parser.rs
@@ -1,67 +1,16 @@
+use self::util::{handle_unexpected, is_identifier, ws, MyError, Res, Span};
 use super::ast::{EnumDecl, SpawnBlock, StatesBlock, StatesBlockElement, ToplevelElement};
 use crate::{Direction, EnemySpawnInfo};
 use nom::{
     branch::alt,
     bytes::complete::{tag, take_till1, take_while},
-    character::complete::{alpha1, alphanumeric1, char, multispace0, one_of},
+    character::complete::{alpha1, alphanumeric1, char, one_of},
     combinator::recognize,
-    error::{ErrorKind, ParseError},
     multi::{many0, many0_count, many1, separated_list0},
     sequence::{delimited, pair, terminated},
-    IResult,
 };
-use nom_locate::LocatedSpan;
 
-pub type Span<'a> = LocatedSpan<&'a str>;
-pub type Res<'a, Output> = IResult<Span<'a>, Output, MyError<Span<'a>>>;
-
-// WIP: error handling inspired by https://www.reddit.com/r/rust/comments/mtmufz/comment/gv18j0a/?utm_source=share&utm_medium=web2x&context=3
-// but much more primitive and simplified by using nom_locate.
-
-#[derive(Debug)]
-pub enum MyError<I> {
-    Nom(nom::error::Error<I>),
-    Custom(String),
-}
-
-impl<I> ParseError<I> for MyError<I> {
-    fn from_error_kind(input: I, kind: ErrorKind) -> Self {
-        // Error { input, code: kind }
-        MyError::Nom(nom::error::Error::from_error_kind(input, kind))
-    }
-
-    fn append(_: I, _: ErrorKind, other: Self) -> Self {
-        other
-    }
-}
-
-fn handle_unexpected<'a, Output, P, F>(
-    parser: P,
-    error_f: F,
-) -> impl Fn(Span<'a>) -> Res<'a, Output>
-where
-    P: Fn(Span<'a>) -> Res<'a, Span<'a>>,
-    F: Fn(Span<'a>) -> MyError<Span<'a>>,
-{
-    use nom::Err::{Error, Failure, Incomplete};
-    move |i: Span<'a>| match parser(i) {
-        Ok((_i_after_parse, parsed)) => Err(Failure(error_f(parsed))),
-        Err(Failure(e)) => Err(Failure(e)),
-        Err(Error(e)) => Err(Error(e)),
-        Err(Incomplete(need)) => Err(Incomplete(need)),
-    }
-}
-
-fn ws<'a, F, O>(inner: F) -> impl FnMut(Span<'a>) -> Res<O>
-where
-    F: FnMut(Span<'a>) -> Res<'a, O>,
-{
-    delimited(multispace0, inner, multispace0)
-}
-
-fn is_identifier(c: char) -> bool {
-    c.is_alphanumeric() || c == '_'
-}
+pub mod util;
 
 fn parse_enum_name(input: Span) -> Res<'_, Span> {
     ws(take_while(is_identifier))(input)
