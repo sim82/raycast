31ffc1f985c4cf4fe275d1e1350f9a2c223d1f01
Simon A. Berger <simberger@gmail.com>
2023-06-09 16:54:37 +0200

    make state compiler optional

--- build.rs
+++ build.rs
@@ -1,8 +1,11 @@
 pub fn main() {
     // state_bc::compiler::compile("states/wl6.st", "src/out.img");
-    state_lang_nt::frontent::compile("states/wl6.st2", "src/out.img");
-    println!("cargo:rerun-if-changed=build.rs");
-    // println!("cargo:rerun-if-changed=states/wl6.st");
-    println!("cargo:rerun-if-changed=states/wl6.st2");
+    #[cfg(feature = "compile_states")]
+    {
+        state_lang_nt::frontent::compile("states/wl6.st2", "src/out.img");
+        println!("cargo:rerun-if-changed=build.rs");
+        // println!("cargo:rerun-if-changed=states/wl6.st");
+        println!("cargo:rerun-if-changed=states/wl6.st2");
+    }
     println!("cargo:rerun-if-changed=Cargo.lock");
 }
