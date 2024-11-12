ee56583debb3a482e89e8578f4aeb76f1713954e
Simon A. Berger <simberger@gmail.com>
2024-11-12 16:53:00 +0100

    shell.nix stuff

--- shell.nix
+++ shell.nix
@@ -1,6 +1,16 @@
-{ pkgs ? import <nixpkgs> {} }:
-  pkgs.mkShell {
-    # nativeBuildInputs is usually what you want -- tools you need to run
-    nativeBuildInputs = with pkgs.buildPackages; 
-    [ SDL2 SDL2_mixer ];
+{ pkgs ? import <nixpkgs> { } }:
+
+with pkgs;
+
+mkShell rec {
+  nativeBuildInputs = [
+    pkg-config
+  ];
+  buildInputs = [
+    udev alsa-lib vulkan-loader
+    xorg.libX11 xorg.libXcursor xorg.libXi xorg.libXrandr # To use the x11 feature
+    libxkbcommon wayland # To use the wayland feature
+    SDL2 SDL2_mixer
+  ];
+  LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
 }
